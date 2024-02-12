98ea235f4bc199081af83981929b45639750eef9
Tariq Ridwan <tariq.ridwan@bsc.es>
2024-02-12 12:43:09 +0100

    added

--- 5_vectors2.cpp
+++ 5_vectors2.cpp
@@ -1,4 +1,4 @@
-/* Slowly getting into Vectors & arrays in C++ . . . starting with vector length calculations . . .
+/* Calculating the vector length using vector
 written by Tariq Ridwan: 👉 https://tariqridwan.github.io/
 Universitat Politècnica de Catalunya, Barcelona */
 
@@ -11,23 +11,24 @@ double vector_length(double x, double y, double z);
 
 int main()
 {
-    double x, y, z, L;
+    double L;
+    double sample_vector[3][1];
     std::string write_intro;
     write_intro = "Calculating Vector length\n";
     std::cout << write_intro;
 
     std::cout << "What is the value of x?" << std::endl;
-    std::cin >> x;
+    std::cin >> sample_vector[0][0];
 
     std::cout << "What is the value of y\n";
-    std::cin >> y;
+    std::cin >> sample_vector[1][0];
 
     std::cout << "What is the value of z?" << std::endl;
-    std::cin >> z;
+    std::cin >> sample_vector[2][0];
 
     // calculate
     std::cout << "So the Vector's Length is:" << std::endl;
-    L = vector_length(x, y, z);
+    L = vector_length(sample_vector[0][0],sample_vector[1][0],sample_vector[2][0]);
     std::cout << L << std::endl;
     return 0;
 }
--- 5_vectors3.cpp
+++ 5_vectors3.cpp
@@ -1,4 +1,4 @@
-/* Slowly getting into Vectors & arrays in C++ . . . starting with vector length calculations . . .
+/* Calculating the vector length using vector, part II
 written by Tariq Ridwan: 👉 https://tariqridwan.github.io/
 Universitat Politècnica de Catalunya, Barcelona */
 
@@ -11,22 +11,31 @@ double vector_length(double x, double y, double z);
 
 int main()
 {
-    double x, y, z, L;
+    double x,y,z,L;
+    double sample_vector[3][1] = {{x},{y},{z}}; // If we want to initialize vectors
+    // double L; // this is another way writing above 2 lines
+    // double x = sample_vector[0][0];
+    // double y = sample_vector[1][0];
+    // double z = sample_vector[2][0];
     std::string write_intro;
     write_intro = "Calculating Vector length\n";
     std::cout << write_intro;
 
     std::cout << "What is the value of x?" << std::endl;
+    // std::cin >> sample_vector[0][0];
     std::cin >> x;
 
     std::cout << "What is the value of y\n";
+    // std::cin >> sample_vector[1][0];
     std::cin >> y;
 
     std::cout << "What is the value of z?" << std::endl;
+    // std::cin >> sample_vector[2][0];
     std::cin >> z;
 
     // calculate
     std::cout << "So the Vector's Length is:" << std::endl;
+    // L = vector_length(sample_vector[0][0],sample_vector[1][0],sample_vector[2][0]);
     L = vector_length(x, y, z);
     std::cout << L << std::endl;
     return 0;
