cc4f8ca69397d6848936bd03aca881491e462cfe
Tariq Ridwan <tariq.ridwan@bsc.es>
2026-09-17 15:41:38 +0200

    cpp4OF ups

--- 2_math0.cpp
+++ 2_math0.cpp
@@ -12,12 +12,12 @@ int main()
     double a,b; // These are two double variables
     float c;
     cout << "Trying to revise the basic code again\n";
-    cout << "Writing a new line to show another way of new line" << endl;
+    cout << "Writing a new line to show another way of new line, BUT ITS NOT THE BEST WAY" << endl;
     cout << "Now let's do some Math.......\n";
     cout << "Let us add two different numbers called a & b.\n";
     cout << "What is the first number?\n";
     cin >> a;
-    cout << "Now by saying bismillah, insert the 2nd number man" << endl;
+    cout << "Now by saying bismillah, insert the 2nd number man:\n";
     cin >> b;
     cout << "Now the last number: \n";
     cin >> c;
--- 2_math1.cpp
+++ 2_math1.cpp
@@ -7,25 +7,30 @@ Universitat Politècnica de Catalunya, Barcelona */
 
 int main()
 {
-    std::cout << "Hello world, this is my first cpp code man!!!\n" << std::endl; // both \n and endl can create new lines
+    std::cout << "Hello world, this is my first cpp code man!!!\n"; // both \n and endl can create new lines
     std::cout << "I am an OpenFOAM user.\nI am learning C++ to be able to modify the OpenFOAM code!!\n";
-    double a = 2, b = 5.2; // These are two double variables
+    int x = 5, y = 10, z = 13; // These are three integer variables
+    double a = 5, b = y; // These are two double variables
     float c;
     std::cout << "Trying to revise the basic code again\n";
-    std::cout << "Writing a new line to show another way of new line" << std::endl;
     std::cout << "Now let's do some Math.......\n";
     std::cout << "Let us add two different numbers called a & b.\n";
     std::cout << "What is the first number?\n";
     std::cout << "a = " << a << std::endl;
     // std::cin >> a;
-    std::cout << "Now by saying bismillah, let's add the 2nd number man" << std::endl;
+    std::cout << "Now by saying bismillah, let's add the 2nd number man\n";
     // std::cin >> b;
     std::cout << "Now the last number: \n";
     std::cin >> c;
+    std::cout << "the user input of c = " << c << std::endl;
     std::cout << "So the summation is: \n";
+    std::cout << x+y+z << std::endl;
+    std::cout << "Sum of integers = " << x+y+z << std::endl;
+    std::cout << "Average of integers = " << (x+y+z)/3 << std::endl;
+    std::cout << "Product of integers = " << x*y*z << std::endl;
     std::cout << a+b+c << std::endl;
-    std::cout << "Sum = " << a+b+c << std::endl;
-    std::cout << "Average = " << (a+b+c)/3 << std::endl;
-    std::cout << "Product = " << a*b*c << std::endl;
+    std::cout << "Sum of doubles = " << a+b+c << std::endl;
+    std::cout << "Average of doubles = " << (a+b+c)/3 << std::endl;
+    std::cout << "Product of doubles = " << a*b*c << std::endl;
     return 0;
 }
