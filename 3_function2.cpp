c9f061faba7a1f92387bd1d015c3603f82f0be8d
Tariq Ridwan <tariq.ridwan@bsc.es>
2026-09-25 10:41:19 +0200

    cpp4OF ups

--- 3_function2.cpp
+++ 3_function2.cpp
@@ -1,4 +1,4 @@
-/* calculating the Reynolds number using a "void" function
+/* calculating the Reynolds number using a "double" function
 written by Tariq Ridwan: 👉 https://tariqridwan.github.io/
 Universitat Politècnica de Catalunya, Barcelona */
 
